22920e393be754826f68acbeb5f96dbb2afcb96e
Valentin Kormanov <vkormanov@initioventures.com>
2016-12-03 13:45:56 -0600

    Added Database Class to make connection with database

---
+++ library/base/Database.php
@@ -0,0 +1,68 @@
+<?php
+include(RDBMS_PATH . "/Mysql.php");
+include(BASE_CLASS_PATH . "/Base.php");
+/**
+ * Database
+ * Extends the base Base class to include a database connection.
+ *
+ * Composition with an class that is wrapper over RDBMS
+ * framework
+ *
+ * @license http://www.opensource.org/licenses/gpl-license.php
+ * @package library
+ * @filesource
+ */
+abstract class Database extends Base
+{   
+    /**
+     * $db
+     *
+     * @var mixed represents database connection
+     */
+    protected $db;
+
+    /**
+     *
+     * @var mixed
+     */
+    protected $type = "mysql";
+
+    /**
+     * __construct
+     *
+     * @access public
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        // switch databases
+        switch ($this->type)
+        {
+            case "mysql":
+            {   // make new Mysql
+                $this->db = new Mysql();
+                break;
+            }
+            default:
+            {
+                throw new Exception("Invalid type");
+                break;
+            }
+        }
+        
+    }// end constructor
+
+    /**
+    * __destruct
+    *
+    * @access public
+    * @return void
+    */
+    public function __destruct()
+    {
+        parent::__destruct();
+    }// end
+}
+
+?>
