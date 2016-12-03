87a11d285ac0246a51e7d6470d8f5194d2732e98
Valentin Kormanov <valentin.kormanov@gmail.com>
2016-12-03 16:45:35 -0600

    Added log file to log certain events

---
+++ log.php
@@ -0,0 +1,20 @@
+<?php
+include(LIBRARY_PATH . '/Logger.php');
+
+/* Log */
+if($config['debug'])
+{
+    $l = new Logger("CONTROLLER.txt");         // make object
+    $l->MakeTitleLine("START CONTROLLER");     // add row
+
+    $h = new Logger("HEADERS.txt");
+    $h->MakeTitleLine("START HEADERS");
+}
+
+
+// log server array
+$l->PrintArray($_SERVER, "SERVER");
+// log post array
+$l->PrintArray($_POST, "POST");
+// log headers
+$h->PrintArray(getallheaders(), "HEADERS");
