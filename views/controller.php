b990918e55c249a245967edd14e16731b7497952
laptop <guve4e@gmail.com>
2017-03-26 09:53:09 -0400

    Added favicon and title to controller view

--- views/controller.php
+++ views/controller.php
@@ -1,4 +1,10 @@
- 
+ <head>
+     <!-- Load favicons -->
+     <link rel="icon" type="image/png" href="views/pics/favicon-16x16.png" sizes="16x16">
+     <link rel="icon" type="image/png" href="views/pics/favicon-32x32.png" sizes="32x32">
+     <!-- Load Title -->
+     <title>Web API V1.0</title>
+ </head>
 <body>
 
 	<h1 align="center" > API </h1>
