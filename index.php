2e6cae2cd9e97a4729db3e443f2fc4e0c7f0b093
home <guve4e@gmail.com>
2020-01-19 18:07:50 -0500

    Fixes index file

--- index.php
+++ index.php
@@ -11,10 +11,18 @@ try
     Logger::logServer();
     Logger::logHeaders();
 
+    if (!isset($_SERVER['PATH_INFO']))
+        throw new NoSuchControllerException("No controller specified!");
+
     new Router(
         new FileManager(),
         new AuthorizationFilter(new FileManager(), new RestCall("Curl", new FileManager())),
         $_SERVER['PATH_INFO']);
+}
+catch (NoSuchControllerException $e)
+{
+    $e->output();
+    include(VIEW_PATH . "/controller.php");
 }
 catch (ApiException $e)
 {
